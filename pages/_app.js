3847be7d0f2786f0ec113a18499d7842ae8baa4c
Junaid Aftab <junaid.aftab@square63.com>
2021-07-15 18:03:27 +0500

    Fix files hierarchy

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/Layout";
 import '../styles/globals.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
