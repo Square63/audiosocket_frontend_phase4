ded2c4fa806244f37c02943f75ec877dd682ad38
Mason Vale <mason.vale@square63.org>
2021-08-03 10:48:52 +0500

    Fixed build error

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a target="_blank" href="https://www.audiosocket.com/">Audiosocket!</a>
+          Welcome to <a rel="noreferrer" target="_blank" href="https://www.audiosocket.com/">Audiosocket!</a>
         </h1>
       </main>
     </div>
