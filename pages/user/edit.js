09ec7fb1c1b28012d5a923a32c38661415f8b6ed
Mason Vale <mason.vale@square63.org>
2021-08-05 17:47:26 +0500

    Added package react-select and country-state-city and implemented country dropdown

--- pages/user/edit.js
+++ pages/user/edit.js
@@ -1,69 +1,91 @@
 import withPrivateRoute from "../../components/withPrivateRoute";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import {useState} from "react";
+import Select from "react-select";
+import { Country } from "country-state-city";
 
-function Edit() {
+function Edit({countries}) {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [countryError, setCountryError] = useState(false);
+
+  const handleSelectCountry = (target) => {
+    if(target.value)
+      setCountryError(false);
+    setSelectedCountry(target.value);
+  }
+
   return (
     <div className="userContent">
       <Form className="profileEdit as-form-control">
         <div className="row">
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicEmail">
+            <Form.Group className="">
               <Form.Label className="required">Name</Form.Label>
               <Form.Control type="text" placeholder="Enter your name" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label className="required">New Email</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>Phone</Form.Label>
               <Form.Control type="text" placeholder="Enter phone" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>organization</Form.Label>
               <Form.Control type="text" placeholder="Enter company name" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>Address</Form.Label>
               <Form.Control type="text" placeholder="Enter here" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>city</Form.Label>
               <Form.Control type="text" placeholder="Enter city" />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label className="required">Country</Form.Label>
-              <Form.Control as="select" custom>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Form.Control>
+              <Select
+                placeholder="Select Country"
+                className="country-select-container-header"
+                classNamePrefix={!countryError ? "country-select-header" : "country-select-header invalid"}
+                options={countries}
+                defaultValue={selectedCountry ? countries.filter(option => option.value === selectedCountry) : {label: "Select Country", value: null}}
+                onChange={handleSelectCountry}
+                noOptionsMessage={() => {return "No country found"}}
+                theme={theme => ({
+                  ...theme,
+                  colors: {
+                    ...theme.colors,
+                    primary: '#c0d72d',
+                  },
+                  height: 34,
+                })}
+              />
             </Form.Group>
           </div>
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>postal code</Form.Label>
               <Form.Control type="text" placeholder="Enter code" />
             </Form.Group>
@@ -71,7 +93,7 @@ function Edit() {
           
 
           <div className="col-lg-6">
-            <Form.Group className="" controlId="formBasicPassword">
+            <Form.Group className="">
               <Form.Label>Youtube URL</Form.Label>
               <Form.Control type="text" placeholder="Enter here" />
             </Form.Group>
@@ -97,3 +119,19 @@ function Edit() {
 }
 
 export default withPrivateRoute(Edit);
+
+export const getStaticProps = () => {
+  const countriesList = Country.getAllCountries();
+  const countries = [];
+  countries.push({label: "Select Country", value: null, countryCode: null});
+  countries.push({label: "United States", value: "United States", countryCode: "US"});
+  countriesList.forEach((country, key) => {
+    if(country.isoCode !== 'US')
+      countries.push({label: country.name, value: country.name, countryCode: country.isoCode})
+  });
+  return {
+    props: {
+      countries
+    }
+  }
+}
