2260e915680fe7bc60a69d442832de29720a8097
Junaid Aftab <junaid.aftab@square63.com>
2021-08-05 14:46:48 +0500

    Styled form component

--- pages/user/edit.js
+++ pages/user/edit.js
@@ -1,88 +1,93 @@
 import withPrivateRoute from "../../components/withPrivateRoute";
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 
 function Edit() {
   return (
     <div className="userContent">
       <Form className="profileEdit as-form-control">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label className="required">Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+          </div>
+
+          <div className="col-lg-6">
+            <Form.Group className="" controlId="formBasicPassword">
+              <Form.Label className="required">New Email</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>Phone</Form.Label>
+              <Form.Control type="text" placeholder="Enter phone" />
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>organization</Form.Label>
+              <Form.Control type="text" placeholder="Enter company name" />
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>Address</Form.Label>
+              <Form.Control type="text" placeholder="Enter here" />
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>city</Form.Label>
+              <Form.Control type="text" placeholder="Enter city" />
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label className="required">Country</Form.Label>
+              <Form.Control as="select" custom>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
             </Form.Group>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-lg-6">
             <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>postal code</Form.Label>
+              <Form.Control type="text" placeholder="Enter code" />
             </Form.Group>
           </div>
-
-          <div className="col-md-6">
-            <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </div>
-
-          <div className="col-md-6">
-            <Form.Group className="" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </div>
-
           
 
-          <div className="col-md-12">
-            <Form.Group className="" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+          <div className="col-lg-6">
+            <Form.Group className="" controlId="formBasicPassword">
+              <Form.Label>Youtube URL</Form.Label>
+              <Form.Control type="text" placeholder="Enter here" />
             </Form.Group>
           </div>
+
           <div className="col-md-12">
-            <Button variant="primary" type="submit">
-              Submit
+            <label>
+              <Form.Label>White-listing enabled</Form.Label>
+              <input type="checkbox"/>
+              <input type="checkbox"/>
+            </label>
+          </div>
+
+          <div className="col-md-12 mt-3">
+            <Button className="userAccountBtn" type="submit">
+              Save
             </Button>
           </div>
         </div>
