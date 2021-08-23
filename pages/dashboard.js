3d51ca80e2814b2ac0e67fca8eaee7f1779009f9
Mason Vale <mason.vale@square63.org>
2021-08-23 17:35:14 +0500

    Removed unnecessary component

--- pages/dashboard.js
+++
@@ -1,9 +0,0 @@
-import withPrivateRoute from "../components/withPrivateRoute"
-
-function Dashboard() {
-  return (
-    <h1>Dashboard page</h1>
-  );
-}
-
-export default withPrivateRoute(Dashboard);
