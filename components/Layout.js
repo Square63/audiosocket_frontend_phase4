b8b9be6ab1872d34aabea0328b3baab750a5dbc3
Mason Vale <mason.vale@square63.org>
2021-08-23 17:49:29 +0500

    Displayed user email from LS on account header and minor style fixes

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,16 @@ import Footer from "./Footer";
 import {AuthProvider} from "../store/authContext";
 import {useRouter} from "next/router";
 import Sidebar from "./Sidebar";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function Layout({children}) {
   const router = useRouter();
+  const [userName, setUserName] = useState('');
 
   useEffect(() => {
+    if(localStorage.getItem('user')) {
+      setUserName(JSON.parse(localStorage.getItem("user") ?? ""));
+    }
     if(process.env.ANALYTICS_WRITE_KEY) {
       let path = router.pathname;
       let title = path === '/' ? path : path.replace('/', '').replaceAll('/',' ');
@@ -27,7 +31,7 @@ function Layout({children}) {
             ?
               <div className="userAccountWrapper">
                 <div className="userAcountHeading">
-                  <h1>Kevin sajjad</h1>
+                  <h1>{userName}</h1>
                 </div>
                 <div className="userAccountInner">
                   <Sidebar/>
