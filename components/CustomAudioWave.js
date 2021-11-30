405bbe114aaea2dfa0aacb7e81885ac373f3ec69
usman sajid <usman.sajid@square63.org>
2021-11-30 16:06:00 +0500

    added tracks in component

--- components/CustomAudioWave.js
+++ components/CustomAudioWave.js
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+
 
 const formWaveSurferOptions = (ref) => ({
   container: ref,
@@ -17,13 +20,12 @@ const formWaveSurferOptions = (ref) => ({
   partialRender: true
 });
 
-export default function CustomAudioWave() {
+export default function CustomAudioWave(props) {
   const waveformRef = useRef(null);
   const wavesurfer = useRef(null);
   const [playing, setPlaying] = useState(false);
 
-  const url =
-    "http://artist-portal-backend-phase4.square63.net//…eb62616ab325994d4e64c19c502/Soft-techno-music.mp3";
+  const url = props.track.file
 
   useEffect(() => {
     create();
@@ -41,7 +43,7 @@ export default function CustomAudioWave() {
     const options = formWaveSurferOptions(waveformRef.current);
     wavesurfer.current = WaveSurfer.create(options);
 
-    wavesurfer.current.load("./test.mp3");
+    wavesurfer.current.load(url);
   };
 
   const handlePlayPause = () => {
@@ -50,20 +52,49 @@ export default function CustomAudioWave() {
   };
 
   return (
-    <div>
-      <div id="waveform" ref={waveformRef}  />
-      <div className="PlayerControls">
-        <div className="startStopBtn" onClick={handlePlayPause}>
-          {!playing ? "Play" : "Pause"}
-          <span className="playSong">
-            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17">
-              <rect id="Rectangle_134" data-name="Rectangle 134" width="5" height="17" fill="#1a1c1d"/>
-              <rect id="Rectangle_135" data-name="Rectangle 135" width="5" height="17" transform="translate(8)" fill="#1a1c1d"/>
-            </svg>
-          </span>
-          <span className="pauseSong d-none"></span>
+    
+    <div style={{display: "contents"}}>
+      <div className="rowParticipant artistName">
+        <div className="playPauseBtn" onClick={handlePlayPause}>
+          <span className={playing ? "play" : "pause"}></span>
+          <span className="pause d-none"></span>
+        </div>
+        <div className="aboutSong">
+          <div className="songData">
+            <a href="" className="songName">{props.track.title}</a>
+            <OverlayTrigger overlay={<Tooltip>Info</Tooltip>}>
+              <a href="" className="info"></a>
+            </OverlayTrigger>
+            <OverlayTrigger overlay={<Tooltip>On Fire (Demo)</Tooltip>}>
+              <a href="" className="fire"></a>
+            </OverlayTrigger>
+            <OverlayTrigger overlay={<Tooltip>Playlist (Demo)</Tooltip>}>
+              <a href="" className="playlistWave"></a>
+            </OverlayTrigger>
+          </div>
+          <div className="songArtist">
+            <a href="">
+              Justin G. Marcellus Abady
+            </a>
+          </div>
         </div>
       </div>
+      <div className="rowParticipant audioWave">
+      <div id="waveform" ref={waveformRef}  />
+        <div className="PlayerControls">
+          <div className="startStopBtn" onClick={handlePlayPause}>
+            {!playing ? "Play" : "Pause"}
+            <span className="playSong">
+              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17">
+                <rect id="Rectangle_134" data-name="Rectangle 134" width="5" height="17" fill="#1a1c1d"/>
+                <rect id="Rectangle_135" data-name="Rectangle 135" width="5" height="17" transform="translate(8)" fill="#1a1c1d"/>
+              </svg>
+            </span>
+            <span className="pauseSong d-none"></span>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
