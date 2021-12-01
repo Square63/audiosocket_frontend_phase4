03018a866abdbcf48d8686b1eaf11bcc5d6d345d
usman sajid <usman.sajid@square63.org>
2021-12-01 13:01:04 +0500

    added common file for all api calls

--- redux/actions/trackActions.js
+++ redux/actions/trackActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // import absoluteUrl from "next-absolute-url";
 import { async } from "regenerator-runtime";
+import {BASE_URL} from "../../common/api";
 
 import {
 	ALL_TRACKS_SUCCESS,
@@ -11,7 +12,7 @@ import {
 export const getTracks = (query, query_type, filters) => async( dispatch ) => {
 	try {
 		// const {origin} = absoluteUrl(req)
-		const {data} = await axios.get(`http://artist-portal-backend-phase4.square63.net/api/v1/consumer/tracks?query=${query}&query_type=${query_type}&filters=${filters}`, {
+		const {data} = await axios.get(`${BASE_URL}/api/v1/consumer/tracks?query=${query}&query_type=${query_type}&filters=${filters}`, {
 			headers: {
 				"Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJhcHBfaWQiOiJhcnRpc3RzLXBvcnRhbC1iYWNrZW5kIn0.etBLEBaghaQBvyYoz1Veu6hvJBZpyL668dfkrRNLla8",
 				"auth-token": "eyJhbGciOiJIUzI1NiJ9.eyJjb25zdW1lcl9pZCI6MSwiZXhwIjoxNjY5NDQ2OTQ1fQ.fRouI5TJ78D-ANMrsGLj7v-u6Y0E1tyej-rGAmulFvw"
