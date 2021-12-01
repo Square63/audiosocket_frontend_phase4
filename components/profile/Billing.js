422f575b2cde79be500222c4e5c1bcebc8526324
Ahmed Raza <ahmed.raza@square63.com>
2021-12-01 18:06:07 +0500

    Separate Billing component for user profile

---
+++ components/profile/Billing.js
@@ -0,0 +1,18 @@
+import withPrivateRoute from "../../components/withPrivateRoute";
+import user from "../../styles/User.module.scss";
+import Link from "next/link";
+
+function Billing() {
+  return (
+    <div className={"userContent " + user.userAccount}>
+      <h1>Billing</h1>
+      <p>
+        {" "}
+        You are not subscribed yet. Please, subscribe{" "}
+        <Link href="/user/subscription">here</Link>.
+      </p>
+    </div>
+  );
+}
+
+export default withPrivateRoute(Billing);
