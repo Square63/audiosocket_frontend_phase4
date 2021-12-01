87155f5eb1c7af29a913eeffeaffe477a2cda0aa
Ahmed Raza <ahmed.raza@square63.com>
2021-12-01 17:12:21 +0500

    Moved profile form structure to its own component

---
+++ components/profile/ProfileForm.js
@@ -0,0 +1,259 @@
+import withPrivateRoute from "../../components/withPrivateRoute";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { Country } from "country-state-city";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useRef, useState } from "react";
+import { useRouter } from "next/router";
+import Select from "react-select";
+
+const ProfileForm = ({ countries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [countryError, setCountryError] = useState(false);
+  const form = useRef(null);
+  const router = useRouter();
+  const [validated, setValidated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSelectCountry = (target) => {
+    if (target.value) setCountryError(false);
+    setSelectedCountry(target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCountryError(false);
+    const loginForm = e.currentTarget;
+    if (loginForm.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      if (!selectedCountry) setCountryError(true);
+      setValidated(true);
+      setIsLoading(false);
+    } else {
+      const data = new FormData(form.current);
+      alert("updated!");
+    }
+  };
+
+  return (
+    <Form
+      noValidate
+      validated={validated}
+      ref={form}
+      onSubmit={handleSubmit}
+      className="profileEdit as-form-control"
+    >
+      <div className="row">
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label className="required">First Name</Form.Label>
+            <Form.Control
+              required
+              name="name"
+              type="text"
+              placeholder="Enter your first name"
+            />
+            <Form.Control.Feedback type="invalid">
+              Name is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label className="required">Last Name</Form.Label>
+            <Form.Control
+              required
+              name="name"
+              type="text"
+              placeholder="Enter your last name"
+            />
+            <Form.Control.Feedback type="invalid">
+              Name is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-12">
+          <Form.Group className="">
+            <Form.Label className="required">Email Address</Form.Label>
+            <Form.Control
+              required
+              name="email"
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Control.Feedback type="invalid">
+              A valid email address is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control name="phone" type="text" placeholder="Enter phone" />
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>Organization</Form.Label>
+            <Form.Control
+              name="organization"
+              type="text"
+              placeholder="Enter company name"
+            />
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>Address</Form.Label>
+            <Form.Control name="address" type="text" placeholder="Enter here" />
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>City</Form.Label>
+            <Form.Control name="city" type="text" placeholder="Enter city" />
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label className="required">State</Form.Label>
+            <Select
+              placeholder="Select State"
+              className={
+                !countryError
+                  ? "react-select-container"
+                  : "react-select-container invalid"
+              }
+              classNamePrefix="react-select"
+              options={countries}
+              defaultValue={
+                selectedCountry
+                  ? countries.filter(
+                      (option) => option.value === selectedCountry
+                    )
+                  : { label: "Select State", value: null }
+              }
+              onChange={handleSelectCountry}
+              noOptionsMessage={() => {
+                return "No state found";
+              }}
+              theme={(theme) => ({
+                ...theme,
+                colors: {
+                  ...theme.colors,
+                  primary: "#c0d72d",
+                },
+                height: 34,
+              })}
+            />
+            {countryError && (
+              <small className="input-error">State is required!</small>
+            )}
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label className="required">Country</Form.Label>
+            <Select
+              placeholder="Select Country"
+              className={
+                !countryError
+                  ? "react-select-container"
+                  : "react-select-container invalid"
+              }
+              classNamePrefix="react-select"
+              options={countries}
+              defaultValue={
+                selectedCountry
+                  ? countries.filter(
+                      (option) => option.value === selectedCountry
+                    )
+                  : { label: "Select Country", value: null }
+              }
+              onChange={handleSelectCountry}
+              noOptionsMessage={() => {
+                return "No country found";
+              }}
+              theme={(theme) => ({
+                ...theme,
+                colors: {
+                  ...theme.colors,
+                  primary: "#c0d72d",
+                },
+                height: 34,
+              })}
+            />
+            {countryError && (
+              <small className="input-error">Country is required!</small>
+            )}
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>Postal code</Form.Label>
+            <Form.Control
+              name="postal_code"
+              type="text"
+              placeholder="Enter code"
+            />
+          </Form.Group>
+        </div>
+
+        <div className="col-lg-6">
+          <Form.Group className="">
+            <Form.Label>Youtube URL</Form.Label>
+            <Form.Control
+              name="youtube_url"
+              type="text"
+              placeholder="Enter here"
+            />
+          </Form.Group>
+        </div>
+
+        <div className="col-md-12 mt-5">
+          <Button className="btn primary-btn" type="submit">
+            Update Profile
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
+};
+
+export default withPrivateRoute(ProfileForm);
+
+export const getStaticProps = () => {
+  const countriesList = Country.getAllCountries();
+  const countries = [];
+  countries.push({ label: "Select Country", value: null, countryCode: null });
+  countries.push({
+    label: "United States",
+    value: "United States",
+    countryCode: "US",
+  });
+  countriesList.forEach((country, key) => {
+    if (country.isoCode !== "US")
+      countries.push({
+        label: country.name,
+        value: country.name,
+        countryCode: country.isoCode,
+      });
+  });
+  return {
+    props: {
+      countries,
+    },
+  };
+};
