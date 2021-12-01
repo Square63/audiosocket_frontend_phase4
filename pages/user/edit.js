51497fcc47043f341022680c06b993c86796deef
Ahmed Raza <ahmed.raza@square63.com>
2021-12-01 17:46:50 +0500

    Attaching countries and states with Edit

--- pages/user/edit.js
+++ pages/user/edit.js
@@ -1,13 +1,14 @@
 import withPrivateRoute from "../../components/withPrivateRoute";
 import ProfileForm from "../../components/profile/ProfileForm";
+import { Country, State } from "country-state-city";
 
-function Edit() {
+function Edit({ countries, states }) {
   return (
     <div className="row">
       <div className="col-lg-6">
         <fieldset>
           <legend>Profile</legend>
-          <ProfileForm />
+          <ProfileForm countries={countries} states={states} />
         </fieldset>
       </div>
       <div className="col-lg-6"></div>
@@ -16,3 +17,37 @@ function Edit() {
 }
 
 export default withPrivateRoute(Edit);
+
+export const getStaticProps = () => {
+  const countriesList = Country.getAllCountries();
+  const countries = [];
+  countries.push({ label: "Select Country", value: null, countryCode: null });
+  countries.push({
+    label: "United States",
+    value: "United States",
+    countryCode: "US",
+  });
+  countriesList.forEach((country, key) => {
+    if (country.isoCode !== "US")
+      countries.push({
+        label: country.name,
+        value: country.name,
+        countryCode: country.isoCode,
+      });
+  });
+  const stateslist = State.getStatesOfCountry("US");
+  const states = [];
+  stateslist.forEach((state, key) => {
+    states.push({
+      label: state.name,
+      value: state.name,
+      stateCode: state.isoCode,
+    });
+  });
+  return {
+    props: {
+      countries,
+      states,
+    },
+  };
+};
