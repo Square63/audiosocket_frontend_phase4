36ebe372f5696b441c43277bd9ee3bbce33ebb6a
Junaid Aftab <junaid.aftab@square63.com>
2021-12-08 15:20:07 +0500

    Profile page sections DOM structure

--- components/profile/Billing.js
+++ components/profile/Billing.js
@@ -1,15 +1,30 @@
 import user from "../../styles/User.module.scss";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Image from 'next/image';
+import masterCard from '../../images/mastercard.svg';
 import Link from "next/link";
 
 function Billing() {
   return (
-    <div className={"userContent " + user.userAccount}>
-      <h1>Billing</h1>
-      <p>
-        {" "}
-        You are not subscribed yet. Please, subscribe{" "}
-        <Link href="/user/subscription">here</Link>.
-      </p>
+    <div className={user.billingInfo}>
+      <div className={user.cardInfoWrapper}>
+        <div className={user.cardInfo}>
+          <div className={user.cardPlate}>
+            <Image src={masterCard} alt="Card Name"/>
+          </div>
+          <div className={user.cardText}>
+            <span className={user.cardNumber}>Ending in 5726</span>
+            <span className={user.cardRank}>Mastercard</span>
+          </div>
+        </div>
+        <OverlayTrigger overlay={<Tooltip>Edit Payment Mode</Tooltip>}>
+          <a href="javascript:void(0)" className={user.editpaymentMode}>&nbsp;</a>
+        </OverlayTrigger>
+      </div>
+      <div className={user.anotherWay}>
+        <a href="javascript:void(0);">Choose another way to pay</a>
+      </div>
     </div>
   );
 }
--- components/profile/PaymentHistory.js
+++ components/profile/PaymentHistory.js
@@ -1,7 +1,30 @@
 import React from "react";
+import user from "../../styles/User.module.scss";
+import Button from "react-bootstrap/Button";
 
 const PaymentHistory = () => {
-  return <div>Payment History</div>;
+  return (
+    <div class={user.paymentHistory}>
+      <ul>
+        <li>
+          <span className={user.paymentDate}>October 5, 2021</span>
+          <a href="javascript:void(0)" className={user.paymentReciept}>view receipt</a>
+        </li>
+        <li>
+          <span className={user.paymentDate}>September 5, 2021</span>
+          <a href="javascript:void(0)" className={user.paymentReciept}>view receipt</a>
+        </li>
+        <li>
+          <span className={user.paymentDate}>August 5, 2021</span>
+          <a href="javascript:void(0)" className={user.paymentReciept}>view receipt</a>
+        </li>
+        <li>
+          <span className={user.paymentDate}>July 5, 2021</span>
+          <a href="javascript:void(0)" className={user.paymentReciept}>view receipt</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default PaymentHistory;
