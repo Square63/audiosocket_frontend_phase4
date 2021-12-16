462737a760bc34f169a583a99652417d8abed44e
Yisal Khan <yisal.khan@square63.org>
2021-12-16 19:30:35 +0500

    did configurations of the toastr

--- common/api.js
+++ common/api.js
@@ -1,1 +1,10 @@
 export const BASE_URL = "http://artist-portal-backend-phase4.square63.net";
+export const TOAST_OPTIONS = {
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+}
