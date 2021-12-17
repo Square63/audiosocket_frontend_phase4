934926c86451bb34569ad735afac507875f78b36
Yisal Khan <yisal.khan@square63.org>
2021-12-17 17:56:18 +0500

    fix build issues

--- pages/selectPlan.js
+++ pages/selectPlan.js
@@ -42,9 +42,9 @@ function SelectPlan() {
             <span>Billing Frequency</span>
             <div className={signup.toggleButton}>
               <input id="toggle-on" className={signup.toggle+" "+signup.toggleLeft} name="toggle" value="false" type="radio" checked/>
-              <label for="toggle-on" className={signup.btn}>Monthly</label>
+              <label htmlFor="toggle-on" className={signup.btn}>Monthly</label>
               <input id="toggle-off" className={signup.toggle+" "+signup.toggleRight} name="toggle" value="true" type="radio"/>
-              <label for="toggle-off" className={signup.btn}>Annually</label>
+              <label htmlFor="toggle-off" className={signup.btn}>Annually</label>
             </div>
             <em>Save 33% with annual billing</em>
           </div>
