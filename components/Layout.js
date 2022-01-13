98d07815144d0f6a2cdb68f8d8ced25cadeff85e
Junaid Aftab <junaid.aftab@square63.com>
2022-01-13 13:53:54 +0500

    Styling of preloader

--- components/Layout.js
+++ components/Layout.js
@@ -39,7 +39,16 @@ function Layout({children}) {
   return (
     <>
       {loading ? (
-        <h1>Loading....</h1>
+        <div className="siteLoader">
+          <h1>Loading....</h1>
+          <div class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+            <div class="rect5"></div>
+          </div>
+        </div>
       ) : (
         <AuthProvider>
           <Header/>
