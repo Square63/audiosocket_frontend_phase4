18af3a0f8a8fd3c58d697b86de77b286e8a82a3f
Junaid Aftab <junaid.aftab@square63.com>
2022-01-24 21:33:19 +0500

    Styled Add to playlist modal and revamp inpage loader

--- components/InpageLoader.js
+++ components/InpageLoader.js
@@ -1,18 +1,32 @@
 function InpageLoader() {
 
   return (
-    <div className="inpageLoader">
-      <div className="audioSocketCube-grid">
-        <div className="audioSocketCube audioSocketCube1"></div>
-        <div className="audioSocketCube audioSocketCube2"></div>
-        <div className="audioSocketCube audioSocketCube3"></div>
-        <div className="audioSocketCube audioSocketCube4"></div>
-        <div className="audioSocketCube audioSocketCube5"></div>
-        <div className="audioSocketCube audioSocketCube6"></div>
-        <div className="audioSocketCube audioSocketCube7"></div>
-        <div className="audioSocketCube audioSocketCube8"></div>
-        <div className="audioSocketCube audioSocketCube9"></div>
-      </div>
+  //   <div className="inpageLoader">
+  //     <div className="audioSocketCube-grid">
+  //       <div className="audioSocketCube audioSocketCube1"></div>
+  //       <div className="audioSocketCube audioSocketCube2"></div>
+  //       <div className="audioSocketCube audioSocketCube3"></div>
+  //       <div className="audioSocketCube audioSocketCube4"></div>
+  //       <div className="audioSocketCube audioSocketCube5"></div>
+  //       <div className="audioSocketCube audioSocketCube6"></div>
+  //       <div className="audioSocketCube audioSocketCube7"></div>
+  //       <div className="audioSocketCube audioSocketCube8"></div>
+  //       <div className="audioSocketCube audioSocketCube9"></div>
+  //     </div>
+  // </div>
+  <div class="sk-fading-circle">
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
   </div>
   );
 }
