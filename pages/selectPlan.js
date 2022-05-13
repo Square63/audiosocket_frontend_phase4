05b6e851fcff46f37571447e67f2e9513788f095
Arsalan Nawaz <arsalan.nawaz@square63.org>
2022-05-13 17:47:44 +0500

    Updated designs for pricing plans in sign up page step 2.

--- pages/selectPlan.js
+++ pages/selectPlan.js
@@ -7,6 +7,8 @@ import Link from "next/link"
 import router, {Router} from "next/router";
 
 import signup from "../styles/Signup.module.scss";
+import PricingPlan from '../components/PricingPlan';
+import SelectPricingPlan from '../components/SelectPricingPlan'
 
 function SelectPlan() {
 
@@ -38,95 +40,8 @@ function SelectPlan() {
               </ul>
             </div>
           </div>
-          <div className="billingFrequency">
-            <span>Billing Frequency</span>
-            <div className="toggleButton">
-              <input id="toggle-on" className="toggle toggleLeft" name="toggle" value="false" type="radio" checked/>
-              <label htmlFor="toggle-on" className="movingBtn">Annually</label>
-              <input id="toggle-off" className="toggle toggleRight" name="toggle" value="true" type="radio"/>
-              <label htmlFor="toggle-off" className="movingBtn">Monthly</label>
-            </div>
-            <em>Save 33% with annual billing</em>
-          </div>
-          <div className={signup.planWrapper}>
-            <div className="boxWithOutShadow">
-              <h2 className={signup.plansHeading}>
-                personal plan
-              </h2>
-              <ul className={signup.planFeatures}>
-                <li><span>Unlimited Licenses</span></li>
-                <li><span>All access to over +80,000 songs</span></li>
-                <li><span>Use for personal and student web media</span></li>
-                <li><span>Cleared for use on your social channels</span></li>
-                <li><span>Monetize on your personal channels.</span></li>
-                <li className={signup.notAvailable}><span>Use for videos or podcasts hosted on company websites or social media channels</span></li>
-                <li className={signup.notAvailable}><span>Monetize on YouTube, Patreon, Twitch and all social platforms</span></li>
-                <li className={signup.notAvailable}><span>CID instant clear codes for your clients</span></li>
-                <li className={signup.notAvailable}><span>Unlimited use in digital ads</span></li>
-              </ul>
-              <div className={signup.plansBtnWrapper}>
-                <a href="javascript:void(0)" className="btn btnMainLarge btnSelectPlan inBlack btn-block mt-0" onClick={() => router.push('/setupPayment')}>
-                  <span className={signup.btnCategory}>Music Only</span><span>$15/mo (billed annually)</span>
-                </a>
-                <a href="javascript:void(0)" className="btn btnMainLarge btnSelectPlan btn-block mt-0" onClick={() => router.push('/setupPayment')}>
-                  <span className={signup.btnCategory}>Music + SFX</span><span>$25/mo (billed annually)</span>
-                </a>
-              </div>
-            </div>
 
-            <div className="boxWithOutShadow">
-              <h2 className={signup.plansHeading}>
-                commercial plan
-              </h2>
-              <ul className={signup.planFeatures}>
-                <li><span>Unlimited Licenses</span></li>
-                <li><span>All access to over +80,000 songs</span></li>
-                <li><span>Use for personal and student web media</span></li>
-                <li><span>Cleared for use on your social channels</span></li>
-                <li><span>Monetize on your personal channels.</span></li>
-                <li><span>Use for videos or podcasts hosted on company websites or social media channels</span></li>
-                <li><span>Monetize on YouTube, Patreon, Twitch and all social platforms</span></li>
-                <li><span>CID instant clear codes for your clients</span></li>
-                <li><span>Unlimited use in digital ads</span></li>
-              </ul>
-              <div className={signup.plansBtnWrapper}>
-                <a href="javascript:void(0)" className="btn btnMainLarge btnSelectPlan inBlack btn-block mt-0" onClick={() => router.push('/setupPayment')}>
-                  <span className={signup.btnCategory}>Music Only</span><span>$59/mo (billed annually)</span>
-                </a>
-                <a href="javascript:void(0)" className="btn btnMainLarge btnSelectPlan btn-block mt-0" onClick={() => router.push('/setupPayment')}>
-                  <span className={signup.btnCategory}>Music + SFX</span><span>$75/mo (billed annually)</span>
-                </a>
-              </div>
-            </div>
-
-            <div className={signup.fullWidthPlans}>
-              <h2 className={signup.plansHeading}>Enterprise Plan</h2>
-              <div className={signup.plansContent}>
-                <p>Need a plan for a large business (more than 100 employees), a team account or for TV, Film, Radio or VOD rights?
-                  Let us customize a plan just for you!</p>
-                <p>Our music is available to license for <strong>ALL MEDIA</strong>. Whatever you’re creating, we’ve got you covered. Just ask!.</p>
-                <p>Please request a custom quote and one of our reps will be in touch ASAP.</p>
-              </div>
-              <div className={signup.plansBtnWrapper}>
-                <a href="javascript:void(0)" className="btn btnMainLarge" onClick={() => router.push('/setupPayment')}>
-                  Request a Custom Quote
-                </a>
-              </div>
-            </div>
-
-            <div className={signup.fullWidthPlans}>
-              <h2 className={signup.plansHeading}>Individual Licensing</h2>
-              <div className={signup.plansContent}>
-                <p>If you’d prefer to pay for track licensing individually, you can create an account without a subscription</p>
-              </div>
-              <div className={signup.plansBtnWrapper}>
-                <a href="javascript:void(0)" onClick={() => router.push('/')} className="btn btnMainLarge">
-                  Continue with Individual Track Licensing
-                </a>
-              </div>
-            </div>
-
-          </div>
+          <SelectPricingPlan></SelectPricingPlan>
         </div>
       </div>
     </div>
