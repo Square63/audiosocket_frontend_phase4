174f0b3b0582ff9cb78ca2c97da6808312e24e3c
usmansajid1122 <37111314+usmansajid1122@users.noreply.github.com>
2022-05-18 13:12:47 +0500

    linked pricing page

--- components/modals/AddToCartLicense.js
+++ components/modals/AddToCartLicense.js
@@ -5,6 +5,8 @@ import Button from "react-bootstrap/Button";
 import Image from "next/image";
 import Loader from "../../images/loader.svg";
 import {AuthContext} from "../../store/authContext";
+import Link from "next/link";
+import router from "next/router";
 
 function AddToCartLicense({showModal = false, onCloseModal, track}) {
   const form = useRef(null);
@@ -98,7 +100,7 @@ function AddToCartLicense({showModal = false, onCloseModal, track}) {
                 <p>
                   For the same price as a single track license, get an unlimited subscription.
                 </p>
-                <a href="javascript:void(0)" className="btn btnMainSmall">View Plans &amp; Pricing</a>
+                <a className="btn btnMainSmall" onClick={()=> {router.push('/pricing')}}>View Plans &amp; Pricing</a>
               </div>
             </div>
           </div>
