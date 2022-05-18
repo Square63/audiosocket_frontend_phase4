fd6fade8b79f6473079cd963a52581ace1b28784
Arsalan Nawaz <arsalan.nawaz@square63.org>
2022-05-18 18:47:05 +0500

    Added rel and key attributes

--- components/modals/AddToCartLicense.js
+++ components/modals/AddToCartLicense.js
@@ -104,7 +104,7 @@ function AddToCartLicense({ showModal = false, onCloseModal, track}) {
             <div className="licensePriceOption">
               <Form className="newThemeRadio roundShape">
                 {['radio'].map((type) => (
-                  <div>
+                  <div key='inline'>
                     {licenses && licenses.map(license =>
                       <Form.Check
                         key={license.id}
