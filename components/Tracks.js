bd77ce583a77f445d25d4b2c76104001f917cf9c
Arsalan Nawaz <arsalan.nawaz@square63.org>
2022-05-25 11:33:09 +0500

    Changed icon for unfollow artist button

--- components/Tracks.js
+++ components/Tracks.js
@@ -424,15 +424,21 @@ function Tracks(props) {
                     { ((localStorage?.getItem('user')) && (followedArtists?.includes(track.artist_id))) ?
                     (<Dropdown.Item>
                       <svg xmlns="http://www.w3.org/2000/svg" width="15.432" height="16.579" viewBox="0 0 15.432 16.579">
-                        <g id="Music-Audio_Modern-Music_modern-music-dj" data-name="Music-Audio / Modern-Music / modern-music-dj" transform="translate(-343.015 -1624.558)">
-                          <g id="Social-Medias-Rewards-Rating_Social-Profile_social-profile-avatar" data-name="Social-Medias-Rewards-Rating / Social-Profile / social-profile-avatar" transform="translate(170.108 1540.602)">
+                        <g id="Music-Audio_Modern-Music_modern-music-dj" data-name="Music-Audio / Modern-Music / modern-music-dj"
+                          transform="translate(-343.015 -1624.558)">
+                          <g id="Social-Medias-Rewards-Rating_Social-Profile_social-profile-avatar"
+                            data-name="Social-Medias-Rewards-Rating / Social-Profile / social-profile-avatar"
+                            transform="translate(170.108 1540.602)">
                             <g id="Group" transform="translate(173.415 84.471)">
                               <g id="social-profile-avatar">
-                                <path id="Shape" d="M182.888,100.035v-2.03h.677a2.03,2.03,0,0,0,2.03-2.03v-2.03h1.9a.338.338,0,0,0,.32-.441c-1.269-3.927-2.186-8.143-6.375-8.909a6.759,6.759,0,0,0-8,5.856,6.583,6.583,0,0,0,2.678,5.935v3.646" transform="translate(-173.415 -84.471)" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
+                                <path id="Shape"
+                                  d="M182.888,100.035v-2.03h.677a2.03,2.03,0,0,0,2.03-2.03v-2.03h1.9a.338.338,0,0,0,.32-.441c-1.269-3.927-2.186-8.143-6.375-8.909a6.759,6.759,0,0,0-8,5.856,6.583,6.583,0,0,0,2.678,5.935v3.646"
+                                  transform="translate(-173.415 -84.471)" fill="none" stroke="#fff" stroke-linecap="round"
+                                  stroke-linejoin="round" stroke-width="1"></path>
                               </g>
                             </g>
-                            <path id="Shape_186" data-name="Shape 186" d="M189.571,568.73V574.8" transform="translate(-9.341 -479.918)" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
-                            <path id="Shape_187" data-name="Shape 187" d="M192.636,571.73h-6.066" transform="translate(-9.373 -479.885)" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
+                            <path id="Shape_187" data-name="Shape 187" d="M192.636,571.73h-6.066" transform="translate(-9.373 -479.885)"
+                              fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"></path>
                           </g>
                         </g>
                       </svg>
