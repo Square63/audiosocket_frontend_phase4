4544de8a81cc00afd3f7fcdc440a22598b701c7c
usmansajid1122 <37111314+usmansajid1122@users.noreply.github.com>
2022-06-08 11:15:43 +0500

    added separate tab for billing

--- components/ProfileMenu.js
+++ components/ProfileMenu.js
@@ -54,6 +54,14 @@ function ProfileMenu() {
           </a>
         </Link>
       </li>
+      <li className={router.pathname.toLowerCase() === "/user/billing" ? "billing selected" : "billing"}>
+        <Link href="/user/billing">
+          <a>
+            <span className="desktopShow">Billing</span>
+            <svg className="mobileShow" xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="200" y1="136" x2="248" y2="136" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><line x1="224" y1="112" x2="224" y2="160" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><circle cx="108" cy="100" r="60" fill="none" stroke="#000000" strokeMiterlimit="10" strokeWidth="16"></circle><path d="M22.2,200a112,112,0,0,1,171.6,0" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path></svg>
+          </a>
+        </Link>
+      </li>
     </ul>
   );
 }
--- pages/user/billing.js
+++ pages/user/billing.js
@@ -1,12 +1,29 @@
 import withPrivateRoute from "../../components/withPrivateRoute";
 import user from "../../styles/User.module.scss";
 import Link from "next/link";
+import BillinSection from "../../components/profile/Billing";
+import PaymentHistory from "../../components/profile/PaymentHistory";
 
 function Billing() {
   return (
-    <div className={'userContent '+user.userAccount}>
-      <h1>Billing</h1>
-      <p> You are not subscribed yet. Please, subscribe <Link href="/user/subscription">here</Link>.</p>
+    <div className="boxDivision">
+      <div className="boxColumn">
+        <div className="boxWithShadow">
+          <div className="boxHeading">
+            Billing Info
+          </div>
+            <BillinSection />
+        </div>          
+      </div>
+      
+      <div className="boxColumn">
+        <div className="boxWithShadow paymentHistoryWrapper">
+          <div className="boxHeading">
+            Payment History
+          </div>
+          <PaymentHistory />
+        </div>
+      </div>
     </div>
   );
 }
