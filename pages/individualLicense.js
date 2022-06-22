ea835d05ca81f48b42b3edd8c59ac63ab0ee175d
Junaid Aftab <junaid.aftab@square63.com>
2022-06-22 14:42:20 +0500

    Fixed typo errors and compress images

--- pages/individualLicense.js
+++ pages/individualLicense.js
@@ -35,20 +35,23 @@ function generalContent() {
         <p>Audiosocket grants to Licensee the non-exclusive license, privilege and authority to copy, perform, edit and/or loop portions of (provided that no such change shall alter the fundamental character of the portion of the Licensed Track), and use the musical composition embedded in the Licensed Tracks in synchronization or timed relation in the Work during the Term throughout the world for the Permitted Purposes set forth in Section 3(c). Notwithstanding the foregoing, the musical compositions contained in the Licensed Tracks shall not be distributed or exploited separately or independently of the Work by the Licensee. Licensee may not sublicense or assign this License.</p>
         <h5> License Restrictions</h5>
         <p>The License granted in Section 3(a) is conditioned upon Licensee&apos;s compliance with the restrictions set forth in this Section 3(b).</p>
-        <p>i. Other Restrictions. Licensee must not use the Licensed Tracks:</p>
         <ul>
-           <li>in any audiovisual work other than the Work;</li>
-           <li>in any advertisement;</li>
-           <li>in any Work made for Hire or use by another party that makes or generates compensation from use of the Licensed Tracks or the Work that includes the Licensed Tracks.;</li>
-           <li>in broadcast, cable, or web television, video games, mobile applications or radio;</li>
-           <li>in theatrical releases;</li>
-           <li>to physically perform or lip-synch the lyrics of the music of the Licensed Track, or in anyway use the artist&apos;s image or likeness except for the purposes of providing artist credit;</li>
-           <li>in pornographic work;</li>
-           <li>in any audiovisual work or any other use that has a political purpose (including, but not limited to, supporting or opposing any government policy, government official, political action, or candidate for political office).</li>
+          <li><strong>Other Restrictions</strong>. &nbsp;Licensee must not use the Licensed Tracks:
+            <ul>
+              <li>in any audiovisual work other than the Work;</li>
+              <li>in any advertisement;</li>
+              <li>in any Work made for Hire or use by another party that makes or generates compensation from use of the Licensed Tracks or the Work that includes the Licensed Tracks.;</li>
+              <li>in broadcast, cable, or web television, video games, mobile applications or radio;</li>
+              <li>in theatrical releases;</li>
+              <li>to physically perform or lip-synch the lyrics of the music of the Licensed Track, or in anyway use the artist&apos;s image or likeness except for the purposes of providing artist credit;</li>
+              <li>in pornographic work;</li>
+              <li>in any audiovisual work or any other use that has a political purpose (including, but not limited to, supporting or opposing any government policy, government official, political action, or candidate for political office).</li>
+            </ul>
+          </li>
         </ul>
         <p>The synchronization license is granted upon the express condition that the musical composition contained in the Licensed Tracks shall not be used to manufacture, sell, license, or exploit sound records or otherwise be used apart from the Work by the Licensee.</p>
 
-        <h5>Permitted Purposes; Distribution.</h5>
+        <h5>Permitted Purposes; Distribution</h5>
         <ul>
           <li>The Work may be a video or background music in an audio podcast that is intended for Individual  distribution on personal channels and student assignments/projects only.</li>
 
@@ -75,7 +78,7 @@ function generalContent() {
         <h3 className="gCSubHeading">8. Credit and Attribution</h3>
         <p>Licensee shall use best efforts to credit Audiosocket and the artist, and include such credit in the final edited version of the Work in each case in which credits for music are included in the Work. No casual or inadvertent failure by Licensee to comply with the credit requirements set forth above, nor any failure by third parties to so comply, shall constitute a breach of this License by Licensee. Recommended language for credit is: &quot;[Name of Licensed Track]&quot; by [Name of Artist], licensed by Audiosocket.&quot; If the Work is posted to YouTube, the credit language should be posted in the video description.</p>
 
-        <h3 className="gCSubHeading">9. Representations and Warranties.</h3>
+        <h3 className="gCSubHeading">9. Representations and Warranties</h3>
         <h5>Audiosocket.</h5> 
         <p>Audiosocket hereby represents and warrants that: (i) it has the power, capacity, and authority to enter into this Agreement; (ii) it has all necessary rights to license the Licensed Tracks to Licensee under the terms of this License; and (iii) Licensee&apos;s exercise of rights hereunder will not infringe any third party&apos;s intellectual property rights.</p>
         <h5>Licensee.</h5>
@@ -91,11 +94,11 @@ function generalContent() {
         <p>This License is governed by the law of the State of Washington (without regard to conflicts of law principles). Any dispute arising out of or relating to this License shall be commenced in the federal or state courts located in King County, Washington. In any such dispute, the substantially prevailing party shall be entitled to its reasonable attorneys&apos; fees.</p>
         <h5>Damages</h5>
         <p>Licensee agrees that damages for the breach of this License are difficult to accurately calculate because of the negative impacts on Audiosocket&apos;s goodwill and reputation in the marketplace and with its artists. Therefore, both parties agree to fix as liquidated damages, and not as a penalty, an amount equal to $25,000 USD, plus Audiosocket&apos;s reasonable attorneys&apos; fees and costs. These liquidated damages are meant to be in addition to Audiosocket&apos;s other remedies, and Licensee agrees they are a reasonable measure of the harm that would be suffered by Audiosocket.</p>
-        <h5>Assignment; Waiver; Modification.</h5> 
+        <h5>Assignment; Waiver; Modification</h5> 
         <p>This License may not be transferred or assigned by Licensee, and is binding on permitted successors and assigns. No modification of this License will be binding unless made in a writing signed by both parties. No waiver of any term, condition or obligation of this License will be valid unless made in writing and signed by the party to which such performance is due. No failure or delay by any party at any time to enforce one or more of the terms, conditions or obligations of this License will (i) constitute waiver of such term, condition or obligation, (ii) preclude such party from requiring performance by the other party at any later time, or (iii) be deemed to be a waiver of any other subsequent term, condition or obligation, whether of like or different nature.</p>
-        <h5>Entire License; Severability.</h5>
+        <h5>Entire License; Severability</h5>
         <p>This License constitutes the entire agreement between the parties with respect to the subject matter hereof and merges all prior and contemporaneous communications. If any provision of this License is held to be invalid or unenforceable to any extent, then the remainder of this License will have full force and effect and such provision will be interpreted, construed or reformed to the extent reasonably required to render the same valid, enforceable and consistent with the original intent underlying such provision.</p>
-        <h5>Notices.</h5>
+        <h5>Notices</h5>
         <p>Any notice of breach or termination hereunder shall be sent via express or registered mail. Notices sent to Audiosocket shall be sent to storefront@audiosocket.com. Notices to Licensee shall be sent to the address provided by Licensee in its online account with Audiosocket. Notices will be deemed delivered upon receipt or three (3) business days after deposit with such mail service.</p>
       </div>
     </div>
