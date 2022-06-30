cd1bdbe3037df49aa84260fcee33d174bd98238b
bilalarshad63 <bilal.arshad@square63.org>
2022-06-30 18:16:03 +0500

    Removed count from creator kits listing

--- pages/playlist/creatorKits.js
+++ pages/playlist/creatorKits.js
@@ -1,27 +1,12 @@
-import user from "../../styles/User.module.scss";
 import playlist from "../../styles/Playlist.module.scss";
 import Image from 'next/image';
-import anime from '../../images/animi.jpeg';
-// import NewPlaylist from "../../components/modals/NewPlaylist";
-import EditPlaylist from "../../components/modals/EditPlaylist";
-import cinemetic from '../../images/cinimetic.jpeg';
-import hiphop from '../../images/hiphop.jpeg';
-import mood1 from '../../images/mood1.png';
-import mood2 from '../../images/mood2.png';
-import mood3 from '../../images/mood3.jpg';
-import mood4 from '../../images/mood4.jpg';
-import Sample1 from '../../images/sample1.jpeg';
-import Sample2 from '../../images/sample2.jpeg';
 import { useDispatch, useSelector } from "react-redux";
 import { getCreatorKits } from "../../redux/actions/authActions";
-import { getFollowedArtists } from "../../redux/actions/authActions";
 import { useState, useEffect } from "react";
 import InpageLoader from '../../components/InpageLoader';
 import Link from "next/link";
 import { ToastContainer, toast } from 'react-toastify';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { BASE_URL } from "../../common/api";
-import axios from "axios";
 import { useRouter } from "next/router";
 import { useCookie } from 'next-cookie'
 
@@ -38,7 +23,6 @@ function CreatorKits() {
   const [creatorKitsArray, setCreatorKitsArray] = useState([])
   const [infifniteLoop, setInfiniteLoop] = useState(false)
   const [hasMore, sethasMore] = useState(true)
-  const [myPlaylistDetail, setMyPlaylistDetail] = useState()
 
   useEffect(() => {
     if (responseStatus == 422) {
@@ -133,24 +117,20 @@ function CreatorKits() {
                     loader={<InpageLoader />}
                     endMessage={<h4>Nothing more to show</h4>}
                   >
-                  <div className="tilesWrapper">
-
-                    {creatorKitsArray &&
-                      creatorKitsArray.map((creatorKit,index)=> {
+                    <div className="tilesWrapper">
+                      {creatorKitsArray && creatorKitsArray.map((creatorKit,index)=> {
                         return(
                           <Link href={"creatorKits/" + creatorKit.id} key={index} onClick={() => {setIsLoading(true)}}>
                             <a key={index} className="tileOverlay">
-                                {creatorKit.playlist_image && <Image src={creatorKit.playlist_image} alt="Mood" className="tilesImg" layout="fill"></Image>}
-                                <span className="tileOverlayText">
-                                  {creatorKit.name}
-                                  <small className="playlistTracksCount">{creatorKit.media_count} Tracks</small>
-                                </span>
-                              </a>
+                              {creatorKit.playlist_image && <Image src={creatorKit.playlist_image} alt="Mood" className="tilesImg" layout="fill"></Image>}
+                              <span className="tileOverlayText">
+                                {creatorKit.name}
+                              </span>
+                            </a>
                           </Link>
-                          )
-                        })}
-
-                  </div>
+                        )
+                      })}
+                    </div>
                   </InfiniteScroll>
                 </section>
               </div>
