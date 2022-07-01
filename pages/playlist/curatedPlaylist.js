2ca9760e8549a4a2e78e7d7ea6b3891e0bd377b2
bilalarshad63 <bilal.arshad@square63.org>
2022-07-01 16:18:48 +0500

    Removed svg from creator kit filter

--- pages/playlist/curatedPlaylist.js
+++ pages/playlist/curatedPlaylist.js
@@ -207,15 +207,6 @@ function CuratedPlaylist() {
                       <Link key={index} href={"creatorKits/" + sub_filter.id}>
                         <div className={selectedFilter == sub_filter.name ? "filterSelf activeFilter" : "filterSelf"} key={index}>
                           <Dropdown.Item href="javascript:void(0)">{sub_filter.name}</Dropdown.Item>
-                          <span className={selectedFilter == sub_filter.name ? "filterControl discardFilter" : "filterControl discardFilter disabled" } onClick={handleClearSingleFilter}>
-                            <svg width="10" height="10" viewBox="0 0 10 10">
-                              <g id="Ellipse_21" data-name="Ellipse 21" fill="none" stroke="#c1d72e" strokeLinejoin="round" strokeWidth="1">
-                                <circle cx="5" cy="5" r="5" stroke="none"/>
-                                <circle cx="5" cy="5" r="4.5" fill="none"/>
-                              </g>
-                              <line id="Line_42" data-name="Line 42" y1="5" x2="5" transform="translate(2.5 2.5)" fill="none" stroke="#c1d72e" strokeWidth="1"/>
-                            </svg>
-                          </span>
                         </div>
                       </Link>
                     )}
