b1cea83f8e4f8ab22189bc5bfd5262615c402310
bilalarshad63 <bilal.arshad@square63.org>
2022-07-04 17:21:22 +0500

    Updated empty search message

--- pages/playlist/creatorKits.js
+++ pages/playlist/creatorKits.js
@@ -24,6 +24,7 @@ function CreatorKits() {
   const [isLoading, setIsLoading] = useState(true);
   const [creatorKitsList, setCreatorKitsList] = useState([])
   const [infifniteLoop, setInfiniteLoop] = useState(false)
+  const [search, setSearch] = useState(true)
   const [hasMore, sethasMore] = useState(true)
 
   useEffect(() => {
@@ -41,9 +42,11 @@ function CreatorKits() {
   }, [responseStatus]);
 
   useEffect(() => {
-    let page;
-    pageNum == 0 ? page = 1 : page = pageNum
-    dispatch(getCreatorKits(searchValue, page))
+    if(search){
+      let page;
+      pageNum == 0 ? page = 1 : page = pageNum
+      dispatch(getCreatorKits(searchValue, page))
+    }
   }, [pageNum]);
 
   useEffect(() => {
@@ -54,6 +57,10 @@ function CreatorKits() {
       setCreatorKitsList(creatorKitsList => [...creatorKitsList, ...creatorKits])
       creatorKitsMeta.total_creator_kits_count == (creatorKitsList.length + creatorKits.length) ? sethasMore(false) : sethasMore(true)
       setIsLoading(false)
+      if (pageNum == 0){
+        setPageNum(1)
+        setSearch(false)
+      }
     }
 
     return () => {
@@ -63,6 +70,7 @@ function CreatorKits() {
 
   const fetchData = () => {
     setPageNum(creatorKitsList.length / 15 + 1)
+    setSearch(true)
     creatorKitsMeta.total_creator_kits_count == creatorKitsList.length ? setInfiniteLoop(false) : setInfiniteLoop(true)
   }
 
@@ -70,68 +78,68 @@ function CreatorKits() {
     setIsLoading(true)
     setCreatorKitsList([])
     setPageNum(0)
+    setSearch(true)
   }
 
   return (
     <>
-      {isLoading ? (
-        <InpageLoader/>) :
-        (
-          <>
-            <div className={playlist.myPlaylistWrapper+' '+playlist.creatorKitsListing}>
-              <ToastContainer
-                position="top-center"
-                autoClose={10000}
-                hideProgressBar
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                style={{ width: "auto" }}
-              />
-              <div className="fixed-container">
-                <section className={playlist.myPlaylists}>
-                  <div className="parallelHead">
-                    <h1>Creator Kits</h1>
-                    <Form className="stickySearch">
-                      <Form.Control type="text" placeholder="Search playlists by title or keyword…" onChange={(e) => setSearchValue(e.target.value)} value={searchValue} />
-                      <Button variant="default" type="submit" className="btnMainLarge stickyBtn" onClick={handleSearch}>Search</Button>
-                    </Form>
-                  </div>
-                  <InfiniteScroll
-                    dataLength={creatorKitsList.length}
-                    next={fetchData}
-                    hasMore={hasMore}
-                    loader={<InpageLoader />}
-                    endMessage={<h4>Nothing more to show</h4>}
-                  >
-                    <div className="tilesWrapper">
-                      {creatorKitsList && creatorKitsList.map((creatorKit,index)=> {
-                        return(
-                          <div key={index} className={playlist.creatorKitsItem}>
-                            <Link href={"creatorKits/" + creatorKit.id} key={index} onClick={() => {setIsLoading(true)}}>
-                              <a className="tileOverlay">
-                                {creatorKit.compressed_playlist_image && <Image src={creatorKit.compressed_playlist_image} alt="Mood" className="tilesImg" layout="fill"></Image>}
-                              </a>
-                            </Link>
-                            <span className="tileOverlayText">
-                              {creatorKit.name}
-                            </span>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </InfiniteScroll>
-                </section>
-              </div>
+      <div className={playlist.myPlaylistWrapper+' '+playlist.creatorKitsListing}>
+        <ToastContainer
+          position="top-center"
+          autoClose={10000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          style={{ width: "auto" }}
+        />
+        <div className="fixed-container">
+          <section className={playlist.myPlaylists}>
+            <div className="parallelHead">
+              <h1>Creator Kits</h1>
+              <Form className="stickySearch">
+                <Form.Control type="text" placeholder="Search playlists by title or keyword…" onChange={(e) => setSearchValue(e.target.value)} value={searchValue} />
+                <Button variant="default" className="btnMainLarge stickyBtn" onClick={handleSearch}>Search</Button>
+              </Form>
             </div>
-          </>
-        )
-      }
+            {isLoading ? (
+              <InpageLoader />) :
+              <InfiniteScroll
+                dataLength={creatorKitsList.length}
+                next={fetchData}
+                hasMore={hasMore}
+                loader={<InpageLoader />}
+                endMessage={<h4>Nothing more to show</h4>}
+              >
+                {creatorKitsList && creatorKitsList.length > 0 ?
+                  <div className="tilesWrapper">
+                    {creatorKitsList.map((creatorKit,index)=> {
+                      return(
+                        <div key={index} className={playlist.creatorKitsItem}>
+                          <Link href={"creatorKits/" + creatorKit.id} key={index} onClick={() => {setIsLoading(true)}}>
+                            <a className="tileOverlay">
+                              {creatorKit.compressed_playlist_image && <Image src={creatorKit.compressed_playlist_image} alt="Mood" className="tilesImg" layout="fill"></Image>}
+                            </a>
+                          </Link>
+                          <span className="tileOverlayText">
+                            {creatorKit.name}
+                          </span>
+                        </div>
+                      )
+                    })}
+                  </div> :
+                  <div className="noContent">No Creator Kit Found</div>
+                }
+              </InfiniteScroll>
+            }
+          </section>
+        </div>
+      </div>
     </>
-  );
+  )
 }
 
 export default CreatorKits;
