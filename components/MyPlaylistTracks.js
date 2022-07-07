27b3227b49feee7bce04eb71cfea5e7175aa7125
bilalarshad63 <bilal.arshad@square63.org>
2022-07-07 11:37:04 +0500

    Fixed bug in lazy loading of playlisyts

--- components/MyPlaylistTracks.js
+++ components/MyPlaylistTracks.js
@@ -43,7 +43,7 @@ function MyPlaylistTracks(props) {
       setTracks(tracks => [...tracks, ...props.tracks])
     setInfiniteLoop(false)
 
-    if (props.tracks.length + tracks.length >= props.myPlaylistTracksMeta.playlist_track_count) {
+    if (props.tracks.length + tracks.length >= props.myPlaylistTracksCount) {
       sethasMore(false)
     } else {
       sethasMore(true)
