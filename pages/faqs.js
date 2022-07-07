ed7a1ba82d7b010afbd771a53a91ae70e9baf0f1
Junaid Aftab <junaid.aftab@square63.com>
2022-07-07 14:19:50 +0500

    Styled FAQ page

---
+++ pages/faqs.js
@@ -0,0 +1,57 @@
+function faqs() {
+  return (
+    <div className="generalContent">
+      <div className="fixed-container">
+        <h1 className="gCHeading">FAQ&apos;s</h1>
+        <h5>What is an Audiosocket Subscription?</h5>
+        <p>An Audiosocket Subscription offers you unlimited licenses during your Subscription, at a very discounted rate. Your licenses are good forever and offer worldwide rights for the defined uses in your Subscription tier. We offer three types of Subscriptions: Personal, Small Business, and Custom.</p>
+
+        <h5>How does the Subscription work?</h5>
+        <p>Once you&apos;ve chosen and purchased the Subscription that works for you, you&apos;re ready to start licensing. Here&apos;s how it works:</p>
+        <ul>
+          <li>Once you&apos;ve found a track you wish to license, add the track to your cart</li>
+          <li>Add your final work title to the Project Name field</li>
+          <li>Click the <strong>License Now</strong> button and your license will be saved in your account under My Licenses</li>
+          <li>Please note this is a very important step, you must do this step to ensure you have the right to use your music in your project</li>
+        </ul>
+        <h5>Can I use any song from Audiosocket in my Subscription?</h5>
+        <p>Yes! When you sign-up, you&apos;ll be given access to everything in the Audiosocket catalog. That&apos;s over 70,000 tracks. Lucky you! <br/> You&apos;re well on your way, discovering the perfect music for your media, all created by trending indie bands and artists from over 200 genres.</p>
+
+        <h5>Can I pay monthly?</h5>
+        <p>Yes! You can choose to be billed monthly or pay an up-front annual rate. When you pay for the year up-front, you get two months free!</p>
+
+        <h5>How long is my license good for?</h5>
+        <p>The licenses you generate during your Subscription are good forever!</p>
+
+        <h5>Why is the price for a single track almost the same as a Subscription?</h5>
+        <p>We believe in loyalty. If you use music from our artists consistently, they make more money over time than they would from a single-use one time license. We believe we have the best Subscription offering on the market, hands down! With over 80,000 high quality tracks across 200 genres from award winning bands, artists and composers, no other Subscription service comes close.</p>
+
+        <h5>Is an Audiosocket Subscription right for me?</h5>
+        <p>If you are consistently creating visual works that need music, or if you&apos;re working on a production that requires a lot of music, a Subscription is right for you! The monthly price is close to that for a single track license and can be paid monthly so you can budget the spend over the course of a year. This makes the service affordable and flexible. Maybe you changed your mind 3 months after you licensed a track, no problem, just drop in a new track, your Subscription covers you!</p>
+
+        <h5>Can I cancel my Subscription?</h5>
+        <p>For monthly subscriptions, you can cancel at any time.  For prepaid annual subscriptions, you may cancel any time, however the effective date would be at the end of your paid term and no refunds are given if you cancel before the end of the term. Your prepaid subscription will automatically renew at the end of the year unless you cancel it with at least 30 days before the renewal date. You may cancel your Subscription at any time by managing your Subscription settings in your account.</p>
+
+        <h5>What happens to my licenses if I cancel my Subscription?</h5>
+        <p>All of the licenses issued to you during your Subscription are perpetual. That means they&apos;re good forever!*  However, you cannot create any new licenses from the songs you downloaded after your Subscription has expired. You must click the License Now button every time you use a track during your Subscription. *Please see our Terms of Service for more info on cancellation.</p>
+
+        <h5>Can I use the same track in multiple projects?</h5>
+        <p>You can use the same track multiple times, BUT you will need a new license for each and every use of a track. Make sure to License Now in checkout for each unique production or you will not legally have the rights needed to use the media. A license is a legal document that is tied to your use through a Project Title.</p>
+
+        <h5>Is my Subscription good for TV?</h5>
+        <p>No, Subscription licenses are only good for web. If you&apos;re interested in television rights, please contact us.</p>
+
+        <h5>How often do you add new songs to your catalog?</h5>
+        <p>We add new songs every single week.</p>
+
+        <h5>What if I need broader rights than those covered in my Subscription?</h5>
+        <p>As a subscriber, you will receive 20% off of any custom license needed for broader rights. You simply submit this form and we&apos;ll get back with you within one business day.</p>
+
+        <h5>Have additional questions?</h5>
+        <p>Please contact us anytime via Email or at 206.701.7931.</p>
+      </div>
+    </div>
+  );
+}
+
+export default faqs;
