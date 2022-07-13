3394a3f7941e40776c70dcc790bcd001ea253c0a
Junaid Aftab <junaid.aftab@square63.com>
2022-07-13 16:05:32 +0500

    Fixed SVG attr

--- components/modals/ShareModal.js
+++ components/modals/ShareModal.js
@@ -31,7 +31,7 @@ function ShareModal({showModal = false, onCloseModal}) {
                 <div className="shareLink">https://prod.audiosocket.com/playlist/curatedPlaylist/1</div>
                 <OverlayTrigger overlay={<Tooltip>Copy Link</Tooltip>}>
                   <div className="copyLink">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="80" y1="128" x2="176" y2="128" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><path d="M104,176H64a48,48,0,0,1,0-96h40" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><path d="M152,176h40a48,48,0,0,0,0-96H152" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="80" y1="128" x2="176" y2="128" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><path d="M104,176H64a48,48,0,0,1,0-96h40" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path><path d="M152,176h40a48,48,0,0,0,0-96H152" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path></svg>
                   </div>
                 </OverlayTrigger>
               </div>
