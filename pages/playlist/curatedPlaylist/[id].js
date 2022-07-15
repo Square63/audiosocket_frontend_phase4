11fb4a5f7afff0b867050e0cae23f5bbbf7ae389
bilalarshad63 <bilal.arshad@square63.org>
2022-07-15 15:57:40 +0500

    Fixed issue in download playlists in zip format

--- pages/playlist/curatedPlaylist/[id].js
+++ pages/playlist/curatedPlaylist/[id].js
@@ -240,6 +240,7 @@ const Details = () => {
         link.download = curatedPlaylistDetail.name;
         link.click();
         setShowDownloadMessage(false)
+        setShowDownloadPlaylist(false)
       } else {
         toast.error("Error while downloading curated playlist.");
       }
@@ -340,7 +341,7 @@ const Details = () => {
                       </svg>
                       Share
                     </Button>
-                        <Button variant="link" className="btn btnMainLarge" onClick={() => setShowDownloadPlaylist(true)}  disabled={curatedPlaylistDetail?.media_count <= 0 || showDownloadMessage}>
+                      <Button variant="link" className="btn btnMainLarge" onClick={() => {setShowDownloadPlaylist(true); handleDownloadZip(query.id);}}  disabled={curatedPlaylistDetail?.media_count <= 0 || showDownloadMessage}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="14.987" height="14.189" viewBox="0 0 14.987 14.189">
                         <g id="icon-download" transform="translate(0.5 13.689) rotate(-90)">
                           <path id="Shape_111" data-name="Shape 111" d="M7.455,2.737V.608A.592.592,0,0,0,6.881,0H.573A.592.592,0,0,0,0,.608V13.379a.592.592,0,0,0,.573.608H6.881a.592.592,0,0,0,.573-.608V11.251" fill="none" stroke="#1a1c1d" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
