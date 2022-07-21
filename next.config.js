50cb6ea6a13ece4ad3199d5da1551e3206836542
usmansajid1122 <37111314+usmansajid1122@users.noreply.github.com>
2022-07-21 11:25:41 +0500

    Added configurstion for image sizes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports =
   {
     images: {
       domains: ['artist-portal-backend-phase4.square63.net', 'manage.audiosocket.com'],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     },
     webpack(config, options) {
       config.module.rules.push({
